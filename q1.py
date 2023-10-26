4bc3b6ccdf22bb53929e9d1ad8b8c854534ddcc9
Jatangitindh <tandonjatan@gmail.com>
2023-10-26 16:04:14 +0530

    correct code

--- q1.py
+++ q1.py
@@ -1,25 +1,23 @@
+def print_super_primes(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
 
-def print(n, isPrime):
-isPrime[0] = isPrime[1] = False
-for i in range(2,n):
-isPrime[i] = True
-for p in range(2,n+1):
-if (p*p<=n and isPrime[p] == True):
-for i in range(p*2,n+1,p):
-isPrime[i] = False
-p += 1
-def superPrimes(n):
-isPrime = [1 for i in range(n+1)]
-prit(n, isPrime)
-primes = [0 for i in range(2,n+1)]
-j = 0
-for p in range(2,n+1):
-if(isPrime[p]):
-primes[j] = p
-j += 1
-for k in range(j):
-if(isPrime[k+1]):
-print (primes[k],end=" ")
+    for p in range(2, n + 1):
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+
+    super_primes = []
+    for p in range(2, n + 1):
+        if is_prime[p]:
+            digits = [int(digit) for digit in str(p)]
+            is_super_prime = all(is_prime[d] for d in digits)
+            if is_super_prime:
+                super_primes.append(p)
+
+    return super_primes
+
 n = 241
-print ("\nSuper-Primes less than or equal to ", n, " are :",)
-superPrimes(n)
+print("Super-Primes less than or equal to", n)
+super_primes = print_super_primes(n)
+print(super_primes)
